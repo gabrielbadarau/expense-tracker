e9e329d61bcd5755e7baafd9d428e19ee5b45eba
Gabriel Badarau <Gabriel.Badarau@endava.com>
2022-12-12 10:35:43 +0200

    Add some icons and create IconRegistryModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IconRegistryModuleModule } from './modules/icon-registry-module/icon-registry-module.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, IconRegistryModuleModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
