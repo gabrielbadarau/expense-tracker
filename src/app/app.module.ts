0bc7c8571d999f141726378f2230e784db5425f6
Gabriel Badarau <Gabriel.Badarau@endava.com>
2022-12-13 18:40:18 +0200

    Setup angular routing & started building login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
 
 import { IconRegistryModule } from './modules/icon-registry-module/icon-registry.module';
 import { MaterialModule } from './modules/material-module/material.module';
 
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent, LoginComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRouting,
     HttpClientModule,
     IconRegistryModule,
     MaterialModule,
--- src/app/modules/material-module/material.module.ts
+++ src/app/modules/material-module/material.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-const allMaterialModules = [CommonModule, MatIconModule];
+const allMaterialModules = [
+  CommonModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+];
 
 @NgModule({
   declarations: [],
