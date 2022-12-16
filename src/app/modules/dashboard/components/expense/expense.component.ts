ef43ef0ec925f5ae233ce589c121c560b9f25e52
Gabriel Badarau <Gabriel.Badarau@endava.com>
2022-12-16 14:36:35 +0200

    Finish expense component UI

--- src/app/modules/dashboard/components/expense/expense.component.ts
+++ src/app/modules/dashboard/components/expense/expense.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
+import { SnackBarComponent } from '../../../../shared/components/snackbar/snackbar.component';
 import { ExpenseCategory } from '../../../../shared/model/expense-category.model';
 
 @Component({
@@ -10,10 +13,44 @@ import { ExpenseCategory } from '../../../../shared/model/expense-category.model
 })
 export class ExpenseComponent {
   maxDateCalendar = new Date();
+  categories = Object.values(ExpenseCategory);
 
-  category = new FormControl<ExpenseCategory | null>(null, Validators.required);
-  date = new FormControl(new Date());
+  form!: FormGroup;
 
-  categories = Object.values(ExpenseCategory)
+  hidePassword = true;
 
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private router: Router) {
+    this.form = this.initForm();
+  }
+
+  create(): void {
+    if (this.form.valid) {
+      console.log(this.form);
+    } else {
+      this.openErrorSnackBar();
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
+  private initForm(): FormGroup {
+    return this.formBuilder.group({
+      date: [{ disabled: true }],
+      category: ['', [Validators.required]],
+      amount: ['', [Validators.required]],
+      description: [''],
+    });
+  }
+
+  private openErrorSnackBar(): void {
+    this.snackBar.openFromComponent(SnackBarComponent, {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      data: 'Check your form errors.',
+      duration: 4000,
+      panelClass: ['error-snackbar'],
+    });
+  }
 }
