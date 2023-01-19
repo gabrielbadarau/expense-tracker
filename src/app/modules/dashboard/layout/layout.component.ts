262521ea1954a27bb22c9beba762a493fb7a64b7
Gabriel Badarau <gabriel.badarau@endava.com>
2023-01-19 10:31:20 +0200

    Added username dynamic

--- src/app/modules/dashboard/layout/layout.component.ts
+++ src/app/modules/dashboard/layout/layout.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { tap, catchError, of } from 'rxjs';
 
 import { SnackBarService } from '../../../shared/services/snackbar.service';
 import { AuthService } from '../../../shared/services/auth.service';
 
+@UntilDestroy()
 @Component({
   selector: 'dashboard-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   isOpen = false;
+  userName!: string | null | undefined;
 
   constructor(private router: Router, private authService: AuthService, private snackBarService: SnackBarService) {}
+
+  ngOnInit(): void {
+    this.authService.getUser$
+      .pipe(
+        untilDestroyed(this),
+        tap((user) => (this.userName = user?.displayName))
+      )
+      .subscribe();
+  }
 
   logout(): void {
     this.authService
