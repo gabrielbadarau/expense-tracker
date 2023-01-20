3fd9040c5df266714b68cb0eb5839283aca3ae76
Gabriel Badarau <gabriel.badarau@endava.com>
2023-01-20 12:04:46 +0200

    Configuring guard and verification email

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
-import { tap, catchError, of } from 'rxjs';
+import { tap, catchError, of, defer, Observable } from 'rxjs';
 
 import { AuthService } from '../../shared/services/auth.service';
 import { SnackBarService } from '../../shared/services/snackbar.service';
@@ -16,6 +16,7 @@ import { SnackBarService } from '../../shared/services/snackbar.service';
 export class VerifyEmailComponent implements OnInit {
   userName: string | null | undefined;
   email: string | null | undefined;
+  sendVerificationEmail!: Observable<any>;
 
   constructor(private snackBarService: SnackBarService, private authService: AuthService, private router: Router) {}
 
@@ -24,15 +25,18 @@ export class VerifyEmailComponent implements OnInit {
       .pipe(
         untilDestroyed(this),
         tap((user) => {
+          this.sendVerificationEmail = defer(async () =>
+            user?.sendEmailVerification({ url: 'https://expensetracker-bd.firebaseapp.com/dashboard' })
+          );
           this.userName = user?.displayName;
           this.email = user?.email;
-
-          if (user?.emailVerified) {
-            this.router.navigate(['/dashboard']);
-          }
         })
       )
       .subscribe();
+  }
+
+  resendEmail(): void {
+    this.sendVerificationEmail.subscribe();
   }
 
   logout(): void {
