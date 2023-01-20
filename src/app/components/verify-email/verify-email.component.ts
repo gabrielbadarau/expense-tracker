d2d671eb6f9b5c8d88392ec621123555c2bd5f78
Gabriel Badarau <gabriel.badarau@endava.com>
2023-01-20 11:12:36 +0200

    Change to verify email page

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { tap, catchError, of } from 'rxjs';
 
 import { AuthService } from '../../shared/services/auth.service';
 import { SnackBarService } from '../../shared/services/snackbar.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
   styleUrls: ['./verify-email.component.scss'],
 })
-export class VerifyEmailComponent {
+export class VerifyEmailComponent implements OnInit {
+  userName: string | null | undefined;
+  email: string | null | undefined;
+
   constructor(private snackBarService: SnackBarService, private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.getUser$
+      .pipe(
+        untilDestroyed(this),
+        tap((user) => {
+          this.userName = user?.displayName;
+          this.email = user?.email;
+        })
+      )
+      .subscribe();
+  }
 
   logout(): void {
     this.authService
