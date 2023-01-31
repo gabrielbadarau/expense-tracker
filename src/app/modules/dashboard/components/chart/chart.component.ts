e3ee51f1eae08695d081723e19f7bc6fb2ae7c29
Gabriel Badarau <gabriel.badarau@endava.com>
2023-01-31 14:16:11 +0200

    So far

--- src/app/modules/dashboard/components/chart/chart.component.ts
+++ src/app/modules/dashboard/components/chart/chart.component.ts
@@ -20,17 +20,58 @@ export class ChartComponent implements OnInit {
       type: 'doughnut',
 
       data: {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: [
+          'Beauty',
+          'Clothing',
+          'Entertainment',
+          'Food',
+          'Gift',
+          'Health',
+          'Household',
+          'Ordered Food',
+          'Self Development',
+          'Social Life',
+          'Transportation',
+          'Other',
+        ],
         datasets: [
           {
-            data: [300, 50, 100],
-            backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
-            hoverOffset: 4,
+            data: [900, 800, 700, 600, 500, 400, 300, 200, 100, 80, 70, 60],
+            backgroundColor: [
+              '#8b9f76',
+              '#97a665',
+              '#a9aa50',
+              '#c1ac39',
+              '#deab20',
+              '#ffa600',
+              '#ff8b45',
+              '#ff786c',
+              '#f4718b',
+              '#d3729f',
+              '#ad75a6',
+              '#89769f',
+            ],
           },
         ],
       },
       options: {
-        aspectRatio: 1,
+        responsive: true,
+
+        plugins: {
+
+          tooltip: {
+            enabled: false,
+          },
+          legend: {
+            display: false,
+          },
+        },
+        elements: {
+          arc: {
+            hoverBorderColor: '#1F2937',
+            hoverBorderWidth: 2,
+          },
+        },
       },
     });
   }
