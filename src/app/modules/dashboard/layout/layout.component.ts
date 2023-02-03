7a045caaf857c6287347566b9b25f3c447f7712b
Gabriel Badarau <gabriel.badarau@endava.com>
2023-02-03 15:11:14 +0200

    Testing analytics

--- src/app/modules/dashboard/layout/layout.component.ts
+++ src/app/modules/dashboard/layout/layout.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { tap, catchError, of } from 'rxjs';
 
 import { SnackBarService } from '../../../shared/services/snackbar.service';
 import { AuthService } from '../../../shared/services/auth.service';
+import { AngularFireAnalytics } from '@angular/fire/compat/analytics';
 
-@UntilDestroy()
 @Component({
   selector: 'dashboard-layout',
   templateUrl: './layout.component.html',
@@ -17,17 +16,36 @@ export class LayoutComponent implements OnInit {
   isOpen = false;
   userName = '';
 
-  constructor(private router: Router, private authService: AuthService, private snackBarService: SnackBarService) {}
+  startTime: number = 0;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private snackBarService: SnackBarService,
+    private analytics: AngularFireAnalytics
+  ) {}
 
   ngOnInit(): void {
     this.userName = this.authService.displayName;
+
+    this.startTime = new Date().getTime();
   }
 
   logout(): void {
     this.authService
       .logout()
       .pipe(
-        tap(() => this.router.navigate(['/login'])),
+        tap(() => {
+          this.router.navigate(['/login']);
+
+          const endTime = new Date().getTime();
+
+          this.analytics.logEvent(`${endTime - this.startTime}`, {
+            userName: this.userName,
+            uid: this.authService.uid,
+            duration: endTime - this.startTime,
+          });
+        }),
         catchError((error) => of(this.snackBarService.openServiceErrorSnackBar(error.message)))
       )
       .subscribe();
